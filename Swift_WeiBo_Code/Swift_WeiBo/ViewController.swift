5868402d514252f15c767f79edf22566298c6ee2
Kyle <zhk108@icloud.com>
2016-08-13 16:30:52 +0800

    新建项目

---
+++ Swift_WeiBo_Code/Swift_WeiBo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_WeiBo
+//
+//  Created by Kyle.Z on 16/8/13.
+//  Copyright © 2016年 kyle.z. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
